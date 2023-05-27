0e8860ed79ba25293534473fab2d2004d9910b09
OrianeSol <solorianebox@gmail.com>
2023-05-27 23:31:34 +0300

    working v27.05

--- game1.js
+++ game1.js
@@ -1,11 +1,21 @@
  
 // JavaScript source code
+
 //timer
 const myInterval = setInterval(myTimer, 1000);
 const later = new Date();
 
+var countered = 0; //make a thing that will check when the colors match
+var counteblue = 0;
+var counteyellow = 0;
+var countergreen = 0;
+
 sec = 10;
-later.setSeconds(later.getSeconds() + sec+2);
+later.setSeconds(later.getSeconds() + sec + 2);
+
+var isTimeUp = false; // Flag to check if the time is up
+var isGameOver = false; // Flag to check if the game is over
+
 function myTimer() {
     const now = new Date();
     var distance = later - now;
@@ -19,99 +29,137 @@ function myTimer() {
     // Update the clock element
     document.getElementById("demo").innerHTML = formattedTime;
 
-    if (distance < 0) {
-        document.getElementById("demo").innerHTML = "youre time is up!";
-        clearInterval(100);
+    if (distance < 0)
+    {
+        document.getElementById("demo").innerHTML = "Your time is up!";
+        clearInterval(myInterval); // Clear the interval and will stop the timer
+        isTimeUp = true; //Set time-up flag
+        checkGameStatus(); // Checking game status
     }
 }
 
-function myStopFunction() {
-    clearInterval(myInterval);
+function goToFrontPage() {
+
+    window.location.href = "color-game(newlinked).html";
 }
 
-//draw canvas
+
 var canvas = document.getElementById("myCanvas");
 
-//report the mouse position on click
-canvas.addEventListener("click", function (evt) {
-    var mousePos = getMousePos(canvas, evt);
-    alert(mousePos.x + ',' + mousePos.y);
-}, false);
 
-//Get Mouse Position
-function getMousePos(canvas, evt) {
-    var rect = canvas.getBoundingClientRect();
-    return {
-        x: evt.clientX - rect.left,
-        y: evt.clientY - rect.top
-    };
+
+function checkGameStatus() {
+
+    // Display a pop-up message
+    
+    if ((!isTimeUp) && (countered === true && counteblue === true && counteyellow === true && countergreen === true))
+    {
+        alert(" Winner! All of the specified images and border colors are in the drop positions!");
+       
+    }
+    else if (isTimeUp && !(countered === true && counteblue === true && counteyellow === true && countergreen === true) )
+    {
+        alert(" Loser! The specified images and border colors are not in the drop positionskGameStatus.");
+    }    
 }
 
 
-
-function allowDrop(ev) {
+function allowDrop(ev)
+{
     ev.preventDefault();
 }
 
-function drag(ev) {
+function drag(ev)
+{
     ev.dataTransfer.setData("text", ev.target.id);
 }
 
-function drop(ev) {
+function drop(ev)
+{
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
-}
 
-//    ctx.addEventListener("drop", drop);
-//    ctx.addEventListener("drag", drag);
-
-
-function Rectangle(x, y, width, height, fillColor, outlineColor) {
-    const ctx = document.getElementById('myCanvas').getContext('2d');
-
-
-
-    ctx.fillStyle = fillColor;
-    ctx.fillRect(x, y, width, height);
-
-    ctx.strokeStyle = outlineColor;
-    ctx.lineWidth = 8;
-    ctx.strokeRect(x + 1, y + 1, width - 2, height - 2);
-
-   
-
-}
-
-function randomColor(colors, lastColors) {
-    let color = colors[Math.floor(Math.random() * colors.length)];
-    while (lastColors.includes(color)) {
-        color = colors[Math.floor(Math.random() * colors.length)];
+    //Check if the drop position contains the image URL+  border color
+    if( ev.target.style.borderColor === "red" &&
+        ev.target.querySelector("img").src === "https://www.color-name.com/color-image?c=FF7779&square"
+    )
+    {
+        countered = true;
     }
-    return color;
+    if( ev.target.style.borderColor === "blue" &&
+        ev.target.querySelector("img").src === "https://www.travelandleisure.com/thmb/KTIha5CLifSoUD3gx0YP51xc3rY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/blue0517-4dfc85cb0200460ab717b101ac07888f.jpg"
+    )
+    {
+        counteblue = true;
+    }
+    if( ev.target.style.borderColor === "gold" &&
+        ev.target.querySelector("img").src === "https://htmlcolorcodes.com/assets/images/colors/pastel-yellow-color-solid-background-1920x1080.png"
+    ) 
+    {
+        counteyellow = true;
+    }
+    if( ev.target.style.borderColor === "limegreen" &&
+        ev.target.querySelector("img").src === "https://htmlcolorcodes.com/assets/images/colors/light-green-color-solid-background-1920x1080.png"
+    )
+    {
+        countergreen = true;
+    }
+  
+    // Check the game status
+    checkGameStatus();
+
 }
 
-const colors = ["red", "green", "blue", "yellow"];
-let lastColors = [];
 
-let color1 = randomColor(colors, lastColors);
-console.log(color1);
-lastColors.push(color1);
 
-let color2 = randomColor(colors, lastColors);
-console.log(color2);
-lastColors.push(color2);
+function getRandomColor(lastColors)
+{
+    var colors = ["blue", "red", "gold", "limegreen"];
+    var availableColors = colors.filter(function (color)
+    {
+        return !lastColors.includes(color);
+    });
+    var randomIndex = Math.floor(Math.random() * availableColors.length);
+    return availableColors[randomIndex];
+}
 
-let color3 = randomColor(colors, lastColors);
-console.log(color3);
-lastColors.push(color3);
+// Get the div elements
+var div1 = document.getElementById('div1');
+var div2 = document.getElementById('div2');
+var div3 = document.getElementById('div3');
+var div4 = document.getElementById('div4');
 
-let color4 = randomColor(colors, lastColors);
-console.log(color4);
-lastColors.push(color4);
+var lastColors = [];
 
-Rectangle(400, 140, 190, 90, color1, color2);
-Rectangle(400, 240, 190, 90, "white", "grey");
-Rectangle(400, 340, 190, 90, color2, color4);
-Rectangle(190, 240, 190, 90, color3, color1);
-Rectangle(610, 240, 190, 90, color4, color3);
+ //Set random outline color for each div
+div1.style.borderColor = getRandomColor(lastColors);
+div1.style.borderColor = getRandomColor(lastColors);
+while (div1.style.borderColor === "red")
+{
+    div1.style.borderColor = getRandomColor(lastColors);
+}
+lastColors.push(div1.style.borderColor);
+
+div2.style.borderColor = getRandomColor(lastColors);
+while (div2.style.borderColor === "blue")
+{
+    div2.style.borderColor = getRandomColor(lastColors);
+}
+lastColors.push(div2.style.borderColor);
+
+div3.style.borderColor = getRandomColor(lastColors);
+while (div3.style.borderColor === "gold")
+{
+    div3.style.borderColor = getRandomColor(lastColors);
+}
+lastColors.push(div3.style.borderColor);
+
+div4.style.borderColor = getRandomColor(lastColors);
+while (div4.style.borderColor === "limegreen")
+{
+    div4.style.borderColor = getRandomColor(lastColors);
+}
+lastColors.push(div4.style.borderColor);
+
+const expectedBorderColor = "red";
